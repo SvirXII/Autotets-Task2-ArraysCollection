dab6ea86f4e2b2e7b3b7eda32a54a6c1e9bc6847
SvirXII <svirxii@yandex.ru>
2018-04-14 00:02:15 +0300

    array collections

---
+++ ArraysCollections/ArraysCollections/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr1 = { "Alex", "Dima", "Kate", "Galina", "Ivan" };
+            string[] arr2 = { "Dima", "Ivan", "Kate" };
+            string[] arr3 = arr1.Except<string>(arr2).ToArray<string>();
+            foreach (string name in arr3)
+            {
+                Console.WriteLine(name);
+            }
+                
+        }
+    }
+}
